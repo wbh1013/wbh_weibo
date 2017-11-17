7077296ad128971d49120c6513a9b50717bfd528
hjy <hjy@hjydeiMac.local>
2017-11-17 17:13:06 +0800

    123

--- WEIBO/WEIBO/Classes/Main/AppDelegate.swift
+++ WEIBO/WEIBO/Classes/Main/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = WBHTableViewController()
+        self.window?.backgroundColor = UIColor.white
+        self.window?.rootViewController = WBHTabBarController()
         self.window?.makeKeyAndVisible()
         return true
     }
